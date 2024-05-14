7b3e481cd5c8764aba2a362fb66cd962ffd95eb1
Santiago Aguirre Giraldo <santiagowe98@gmail.com>
2024-05-13 21:34:39 -0500

    Detail product page styles improved

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
 import { SanitizeHTML } from "app/components/shared/SanitizeHTML";
-import styles from './ProductView.module.sass'
+import styles from "./ProductView.module.sass";
 
 interface ProductViewProps {
-  product: ProductType
+  product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
-
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
@@ -24,14 +23,15 @@ export const ProductView = ({ product }: ProductViewProps) => {
       <section className={styles.ProductView__info}>
         <h1 className={styles.ProductView__info__title}>{product.title}</h1>
         <p className={styles.ProductView__info__category}>{product.tags}</p>
-        <SanitizeHTML tag='p'>
-          {product.description}
-        </SanitizeHTML>
+        <SanitizeHTML tag="p">{product.description}</SanitizeHTML>
         <span className={styles.ProductView__info__price}>
           $ {product.price}
         </span>
-        <ProductViewItemsOrder maxQuantity={product.quantity} product={product} />
+        <ProductViewItemsOrder
+          maxQuantity={product.quantity}
+          product={product}
+        />
       </section>
     </main>
-  )
+  );
 };
