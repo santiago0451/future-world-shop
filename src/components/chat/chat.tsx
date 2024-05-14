b716794048eecfbd83eb082d7a4c1dd61ebd3472
Santiago Aguirre Giraldo <santiagowe98@gmail.com>
2024-05-13 21:24:02 -0500

    Styles of the chat changed and improved

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,20 +1,21 @@
-'use client';
-import { useChat } from 'ai/react';
-import styles from './Chat.module.sass';
+"use client";
+
+import { useChat } from "ai/react";
+import styles from "./Chat.module.sass";
 
 export const Chat = (props: { agent: string }) => {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     initialMessages: [
       {
-        id: '1',
-        role: 'system',
-        content: props.agent
-      }
+        id: "1",
+        role: "system",
+        content: props.agent,
+      },
     ],
   });
 
   return (
-    <main className={styles.Chat} >
+    <main className={styles.Chat}>
       <h1 className={styles.Chat__title}>Ask anything, buy everything</h1>
       <form onSubmit={handleSubmit} className={styles.Chat__form}>
         <input
@@ -23,30 +24,26 @@ export const Chat = (props: { agent: string }) => {
           onChange={handleInputChange}
           placeholder="What would you like to buy?"
         />
-        <button
-          className={styles.Chat__button}
-        >
-          Send
-        </button>
+        <button className={styles.Chat__button}>Send</button>
       </form>
       <section className={styles.Chat__messages}>
         {messages
-          .filter(m => m.role !== 'system')
-          .map(m => {
+          .filter((m) => m.role !== "system")
+          .map((m) => {
             return (
               <span key={m.id} className={styles.Chat__message}>
                 <div className={styles.Chat__message__icon}>
                   {m.role === "assistant" ? "🤖" : "😊"}
                 </div>
-                <div>
-                  {m.content}
-                </div>
+                <div>{m.content}</div>
               </span>
-            )
-          }
-
-          )}
+            );
+          })}
       </section>
+      <p className={styles.Chat__info}>
+        If you don't get any response, it's because the Open AI quota is already
+        exceeded. 😅
+      </p>
     </main>
   );
-}
+};
